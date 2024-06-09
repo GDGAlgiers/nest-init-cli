56bb3e5bd6ee56843a9c4ced4b4a94f3b4d50def
066516 <ghmamdjeridinabil@gmail.com>
2024-06-09 18:40:26 +0100

    add type orm with sql and add install dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,9 @@ import { CommandRunnerModule } from 'nest-commander';
 import { PrismaConfigCommand } from './commands/prisma-config.command';
 import { PackageManagerService } from './utils/packageManager.service';
 import { FileManagerService } from './utils/fileManager.service';
-import { TypeOrmComfigCommand } from './commands/typeOrm-config.command';
+import { TypeOrmConfigCommand } from './commands/typeOrm-config.command';
 @Module({
-providers:[PrismaConfigCommand,TypeOrmComfigCommand, PackageManagerService, FileManagerService],
+providers:[PrismaConfigCommand,TypeOrmConfigCommand, PackageManagerService, FileManagerService],
   imports: [CommandRunnerModule],
 })
 export class AppModule {}
--- src/commands/typeOrm-config.command.ts
+++ src/commands/typeOrm-config.command.ts
@@ -1,98 +1,98 @@
 /* eslint-disable prettier/prettier */
 import { Command, CommandRunner, Option } from 'nest-commander';
-import { exec } from 'child_process';
 import { Spinner } from 'cli-spinner';
 import { join } from 'path';
 import { PackageManagerService } from '../utils/packageManager.service';
 import { writeFile } from 'fs/promises';
 import { FileManagerService } from 'src/utils/fileManager.service';
-import readline from 'readline';
-import * as fs from 'fs';
-@Command({ name: 'install-typeOrm', description: 'Install Prisma test' })
-export class TypeOrmComfigCommand extends CommandRunner {
-    constructor(
-        private readonly packageManagerService: PackageManagerService,
-        private readonly fileManagerService: FileManagerService,
-        private type:string
 
-    ) {
-        super();
-    }
-    private  envFileContenu = `{
-  "type": "${this.type}",
-  "host": "localhost",
-  "port": 5432,
-  "username": "user",
-  "password": "password",
-  "database": "mydb",
-  "entities": ["dist/**/*.entity{.ts,.js}"],
-  "migrations": ["dist/migration/*.js"],
-  "cli": {
-    "migrationsDir": "src/migration"
+@Command({ name: 'install-typeorm', description: 'Install TypeORM' })
+export class TypeOrmConfigCommand extends CommandRunner {
+  private envFileContent = ``;
+
+  constructor(
+    private readonly packageManagerService: PackageManagerService,
+    private readonly fileManagerService: FileManagerService,
+  ) {
+    super();
   }
-}`
-  
-      
-        async run(
-            passedParams: string[],
-            options?: Record<string, any>,
-          ): Promise<void> {
-            try {
-              if (
-                !options?.flag ||
-                options?.flag === '-m' ||
-                options?.flag === '--mongodb' ||
-                options?.flag === '-psql' ||
-                options?.flag === '--postgresql'
-              ) {
-                // this.installPrismaDependencies();
-                
-                await writeFile(
-                  join(process.cwd(), '', '.env'),
-                  this.envFileContenu,
-                );
-              
-                console.log("env file added succefully")
-               
-              } else {
-                console.log(
-                  'Please provide a valid flag -m for mongodb and -psql for postgresql',
-                );
-              }
-            } catch (err) {
-              console.error(err);
-            }
-          }
-          @Option({
-            flags: '-m, --mongodb',
-            description: 'config typeOrm with mongo-db',
-          })
-        async runWithMongo() {
-           
-            console.log('Configuring type Orm with MongoDB...');
-            this.envFileContenu = `{
-              type: 'mongodb',
-              url: 'mongodb://localhost:27017/nest', // Your MongoDB connection string
-              useNewUrlParser: true,
-              useUnifiedTopology: true,
-              synchronize: true, // Set to false in production
-              entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            }`
-            await writeFile(
-              join(process.cwd(), '', '.env'),
-              this.envFileContenu,
-            );
-            console.log('type orm with mongo configured  successfully!');
 
-          }
-         
-          private installPrismaDependencies(): void {
-            const spinner = new Spinner('Installing Prisma... %s');
-            spinner.setSpinnerString('|/-\\');
-            spinner.start();
-            this.packageManagerService.installDependency('prisma', true);
-            this.packageManagerService.installDependency('@prisma/client');
-            spinner.stop(true);
-            console.log('Prisma installed successfully!');
-          }
+  async run(
+    passedParams: string[],
+    options?: Record<string, any>,
+  ): Promise<void> {
+    try {
+      if (
+        !options?.flag ||
+        options?.flag === '-m' ||
+        options?.flag === '--mongodb' ||
+        options?.flag === '-psql' ||
+        options?.flag === '--postgresql'
+      ) {
+     await   this.installTypeOrmDependencies();
+       
+        // const importPrisma = `import { PrismaService } from './prisma.service'; `;
+        // const prismaProvider ="PrismaService"
+
+      //  await this.fileManagerService.addProviderToAppModule(importPrisma,prismaProvider);
+        console.log("type orm configured succefully")
+       
+      } else {
+        console.log(
+          'Please provide a valid flag -m for mongodb and -psql for postgresql',
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  @Option({
+    flags: '--mongodb',
+    description: 'Configure TypeORM with MongoDB',
+  })
+  async runWithMongo() {
+    console.log('Configuring TypeORM with MongoDB...');
+    this.envFileContent = `
+    TYPE=mongodb
+    URL=mongodb://localhost:27017/nest
+    USE_NEW_URL_PARSER=true
+    USE_UNIFIED_TOPOLOGY=true
+    SYNCHRONIZE=true
+    ENTITIES=__dirname + '/**/*.entity{.ts,.js}'
+  `;
+    await writeFile(join(process.cwd(), '.env'), this.envFileContent);
+    console.log('TypeORM with MongoDB configured successfully!');
+  }
+
+  @Option({
+    flags: '--postgresql',
+    description: 'Configure TypeORM with PostgreSQL',
+  })
+  async runWithSql() {
+    console.log('Configuring TypeORM with PostgreSQL...');
+    this.envFileContent = `
+      TYPE=postgres
+      HOST=localhost
+      PORT=5432
+      USERNAME=user
+      PASSWORD=password
+      DATABASE=mydb
+      ENTITIES=dist/**/*.entity{.ts,.js}
+      MIGRATIONS=dist/migration/*.js
+      CLI_MIGRATIONS_DIR=src/migration
+    `;
+    await writeFile(join(process.cwd(), '.env'), this.envFileContent);
+    console.log('TypeORM with PostgreSQL configured successfully!');
+  }
+
+  private async installTypeOrmDependencies(): Promise<void> {
+    const spinner = new Spinner('Installing TypeORM... %s');
+    spinner.setSpinnerString('|/-\\');
+    spinner.start();
+    await this.packageManagerService.installDependency('typeorm', true);
+    await this.packageManagerService.installDependency('@nestjs/typeorm');
+    spinner.stop(true);
+    console.log('TypeORM installed successfully!');
+  }
 }
