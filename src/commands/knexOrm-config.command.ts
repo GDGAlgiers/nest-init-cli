d819ebf8f49f96deefcffcdd00e9cbffabb3f841
066516 <ghmamdjeridinabil@gmail.com>
2024-06-14 22:36:36 +0100

    seperate modules content in other file

--- src/commands/knexOrm-config.command.ts
+++ src/commands/knexOrm-config.command.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import { PackageManagerService } from '../utils/packageManager.service';
 import { FileManagerService } from 'src/utils/fileManager.service';
 import { writeFile } from 'fs/promises';
+import { knexMySQLModuleContent, knexPostgresModuleContent } from 'src/module-content/knexOrm.content';
 
 @Command({ name: 'install-knexORm', description: 'Install knex Orm' })
 export class KnexOrmConfigCommand extends CommandRunner {
@@ -84,41 +85,7 @@ export class KnexOrmConfigCommand extends CommandRunner {
       
         if (flag === '-psql' || flag === '--postgresql') {
           filename = 'knex.postgresql.module.ts';
-          moduleContent = `import * as Knex from 'knex';
-import { Module } from '@nestjs/common';
-
-export const knexProvider = {
-  provide: 'KnexConnection',
-  useFactory: async () => {
-    const knexConnection = Knex({
-      client: 'pg',
-      connection: {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: '20032003',
-        database: 'test',
-      },
-    });
-
-    try {
-      await knexConnection.raw('SELECT 1'); // Test connection
-      console.log('Knex connected to PostgreSQL database successfully');
-    } catch (error) {
-      console.error('Error connecting to PostgreSQL database:', error);
-      throw error;
-    }
-
-    return knexConnection;
-  },
-};
-
-@Module({
-  providers: [knexProvider],
-  exports: ['KnexConnection'],
-})
-export class KnexPostgresModule {}
-          `;
+          moduleContent = knexPostgresModuleContent
         }
       
         try {
@@ -145,41 +112,7 @@ export class KnexPostgresModule {}
     
       if (flag === '-my' || flag === '--mysql') {
         filename = 'knex.mysql.module.ts';
-        moduleContent = `import * as Knex from 'knex';
-          import { Module } from '@nestjs/common';
-          
-          export const knexProvider = {
-            provide: 'KnexConnection',
-            useFactory: async () => {
-              const knexConnection = Knex({
-                client: 'mysql2',
-                connection: {
-                  host: 'localhost',
-                  port: 3306,
-                  user: 'your_mysql_username',
-                  password: 'your_mysql_password',
-                  database: 'your_mysql_database',
-                },
-              });
-          
-              try {
-                await knexConnection.raw('SELECT 1'); // Test connection
-                console.log('Knex connected to MySQL database successfully');
-              } catch (error) {
-                console.error('Error connecting to MySQL database:', error);
-                throw error;
-              }
-          
-              return knexConnection;
-            },
-          };
-          
-          @Module({
-            providers: [knexProvider],
-            exports: ['KnexConnection'],
-          })
-          export class KnexORmMySqlModule {}
-        `;
+        moduleContent = knexMySQLModuleContent
       }
     
       try {
