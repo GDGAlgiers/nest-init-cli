dcce11c2338a4157e960a82051ca792dbe41f3d0
Ninou01 <ninofcm20@gmail.com>
2024-06-15 01:03:10 +0100

    creating user module after the execution of mongoose-install

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
-  /* eslint-disable prettier/prettier */
- import { Module } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
 import { CommandRunnerModule } from 'nest-commander';
 import { PrismaConfigCommand } from './commands/prisma-config.command';
 import { PackageManagerService } from './utils/packageManager.service';
 import { FileManagerService } from './utils/fileManager.service';
 import { TypeOrmConfigCommand } from './commands/typeOrm-config.command';
+import { MongooseConfigCommand } from './commands/mongoose-config.command';
+import { CommandExecutionService } from './utils/commandExecutionService.service';
 @Module({
-providers:[PrismaConfigCommand,TypeOrmConfigCommand, PackageManagerService, FileManagerService],
-imports:[  CommandRunnerModule],
+  providers:[PrismaConfigCommand, MongooseConfigCommand, TypeOrmConfigCommand, PackageManagerService, FileManagerService, CommandExecutionService],
+  imports:[CommandRunnerModule],
 })
 export class AppModule {}
