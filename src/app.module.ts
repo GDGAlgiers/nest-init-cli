b2f3e843cff30fd84f1deef1facb4fe9f7fa1bcd
wissalcodes <lw_messikh@esi.dz>
2024-06-19 08:01:02 +0100

    added MikroORM config cmd with MySQL, PostgreSQL and MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
-   /* eslint-disable prettier/prettier */
- import { Module } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
 import { CommandRunnerModule } from 'nest-commander';
 import { PrismaConfigCommand } from './commands/prisma-config.command';
 import { PackageManagerService } from './utils/packageManager.service';
 import { FileManagerService } from './utils/fileManager.service';
 import { TypeOrmConfigCommand } from './commands/typeOrm-config.command';
+import { MikroOrmConfigCommand } from './commands/mikro-orm-config.command';
 @Module({
-providers:[PrismaConfigCommand,TypeOrmConfigCommand, PackageManagerService, FileManagerService],
-imports:[  CommandRunnerModule,],
+  providers: [
+    MikroOrmConfigCommand,
+    PrismaConfigCommand,
+    TypeOrmConfigCommand,
+    PackageManagerService,
+    FileManagerService,
+  ],
+  imports: [CommandRunnerModule],
 })
 export class AppModule {}
--- src/utils/packageManager.service.ts
+++ src/utils/packageManager.service.ts
@@ -10,24 +10,65 @@ export class PackageManagerService {
     const files = await fs.promises.readdir(process.cwd());
     const hasNpmLockFile = files.includes('package-lock.json');
     if (hasNpmLockFile) {
-      return 'npm'
+      return 'npm';
     }
     const hasYarnLockFile = files.includes('yarn.lock');
     if (hasYarnLockFile) {
-      return 'yarn'
+      return 'yarn';
     }
 
     const hasPnpmLockFile = files.includes('pnpm-lock.yaml');
     if (hasPnpmLockFile) {
-      return 'pnpm'
+      return 'pnpm';
     }
-    
+
     return 'unknown';
   }
 
-  async installDependency(dependency: string, dev = false) {
+  async installDependency(dependency: string, dev = false): Promise<void> {
     const packageManager = await this.detectPackageManager();
-    const command = `${packageManager} add ${dependency} ${dev ? '--save-dev' : ''}`;
-    exec(command);
+    const command = `${packageManager} add ${dependency} ${
+      dev ? '--save-dev' : ''
+    }`;
+    return new Promise((resolve, reject) => {
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error installing ${dependency}:`, stderr);
+          reject(new Error(`Error installing ${dependency}: ${stderr}`));
+        } else {
+          console.log(`${dependency} installed successfully:`, stdout);
+          resolve();
+        }
+      });
+    });
+  }
+
+  async installDependencyVersion(
+    dependency: string,
+    version: string,
+    dev = false,
+  ): Promise<void> {
+    const packageManager = await this.detectPackageManager();
+    const versionArg = version ? `@${version}` : '';
+    const command = `${packageManager} add ${dependency}${versionArg} ${
+      dev ? '--save-dev' : ''
+    }`;
+
+    return new Promise((resolve, reject) => {
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error installing ${dependency}@${version}:`, stderr);
+          reject(
+            new Error(`Error installing ${dependency}@${version}: ${stderr}`),
+          );
+        } else {
+          console.log(
+            `${dependency}@${version} installed successfully:`,
+            stdout,
+          );
+          resolve();
+        }
+      });
+    });
   }
 }
