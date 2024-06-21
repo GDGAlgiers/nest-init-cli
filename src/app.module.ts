5d17a2c6183553a35d6c6075dfd850557c3bbbfe
Rayan allali <108684980+Rayan-Allali@users.noreply.github.com>
2024-06-21 12:15:40 +0100

    Revert "Nabil/knex"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
-    /* eslint-disable prettier/prettier */
+   /* eslint-disable prettier/prettier */
  import { Module } from '@nestjs/common';
 import { CommandRunnerModule } from 'nest-commander';
 import { PrismaConfigCommand } from './commands/prisma-config.command';
 import { PackageManagerService } from './utils/packageManager.service';
 import { FileManagerService } from './utils/fileManager.service';
 import { TypeOrmConfigCommand } from './commands/typeOrm-config.command';
-import { KnexOrmConfigCommand } from './commands/knexOrm-config.command';
 @Module({
-providers:[PrismaConfigCommand,TypeOrmConfigCommand,KnexOrmConfigCommand, PackageManagerService, FileManagerService],
-imports:[ CommandRunnerModule,],
+providers:[PrismaConfigCommand,TypeOrmConfigCommand, PackageManagerService, FileManagerService],
+imports:[  CommandRunnerModule,],
 })
 export class AppModule {}
